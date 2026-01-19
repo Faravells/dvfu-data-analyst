6f76587e125ff8abcce9015a29b7b430c723b21f
Faravells <139291273+Faravells@users.noreply.github.com>
2026-01-19 18:24:17 +1000

    Added data bounds

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ import time
 
 temperature = 10.25 #градусы
 humidity = 60.35 #проценты
-pressure = 790.45 #мм.рт.ст.
+pressure = 760.45 #мм.рт.ст.
 windSpeed = 20.75 #км/ч
 while True:
-    temperature += round(random.uniform(-0.05, 0.05), 2)
-    humidity += round(random.uniform(-0.05, 0.05), 2)
-    pressure += round(random.uniform(-0.05, 0.05), 2)
-    windSpeed += round(random.uniform(-0.1, 0.1), 2)
+    temperature = round(temperature + random.uniform(-0.05, 0.05), 2)
+    if temperature > 15:
+        temperature -= 0.1
+    if temperature < 5:
+        temperature += 0.1
+    humidity = round(humidity + random.uniform(-0.05, 0.05), 2)
+    if humidity > 100:
+        humidity -= 0.1
+    if humidity < 0:
+        humidity += 0.1
+    pressure = round(pressure + random.uniform(-0.05, 0.05), 2)
+    if pressure > 790:
+        pressure -= 0.1
+    if pressure < 720:
+        pressure += 0.1
+    windSpeed = round(windSpeed + random.uniform(-0.1, 0.1), 2)
+    if windSpeed > 50:
+        windSpeed -= 0.2
+    if windSpeed < 0:
+        windSpeed += 0.2
     time.sleep(1)

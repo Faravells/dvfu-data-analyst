fade206474e4aa3a9460296289012dfd58536d71
Faravells <139291273+Faravells@users.noreply.github.com>
2026-01-19 18:13:17 +1000

    Added random fluctuation

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import random
+
 temperature = 10.25 #градусы
 humidity = 60.35 #проценты
 pressure = 790.45 #мм.рт.ст.
 windSpeed = 20.75 #км/ч
+while True:
+    temperature += round(random.uniform(-0.05, 0.05), 2)
+    humidity += round(random.uniform(-0.05, 0.05), 2)
+    pressure += round(random.uniform(-0.05, 0.05), 2)
+    windSpeed += round(random.uniform(-0.1, 0.1), 2)

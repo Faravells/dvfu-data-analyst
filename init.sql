9fbcef8d4ce32136277ed8025d0af69f75efb9f1
Faravells <139291273+Faravells@users.noreply.github.com>
2026-01-20 12:37:55 +1000

    Fixed AUTO_INCREMENT tag

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS weather_station;
 USE weather_station;
 CREATE TABLE IF NOT EXISTS weather (
-    id INT AUTOINCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     temperature FLOAT(4,2) NOT NULL,
     humidity FLOAT(5,2) NOT NULL,
     pressure FLOAT(5,2) NOT NULL,

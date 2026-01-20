bd6ba43c76ea6924d942947f6a09ff6fb4524e6f
Faravells <139291273+Faravells@users.noreply.github.com>
2026-01-20 12:19:38 +1000

    Added database connection

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
 import time
+import mysql.connector
+import os
 
 temperature = 10.25 #градусы
 humidity = 60.35 #проценты
 pressure = 760.45 #мм.рт.ст.
 windSpeed = 20.75 #км/ч
+
+connection = None
+try:
+    connection = mysql.connector.connect(host=os.getenv('DB_HOST'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'),
+                                         database=os.getenv('DB_NAME'), port=os.getenv('DB_PORT'))
+    print("Генератор данных подключен к базе данных")
+except mysql.connector.Error as e:
+    print(f"Генератор данных не подключен к базе данных: {e}")
 while True:
     temperature = round(temperature + random.uniform(-0.05, 0.05), 2)
     if temperature > 15:
